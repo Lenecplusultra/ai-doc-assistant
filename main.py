ad42bb8794ed4f5e4869850f7134b409fe99ddfd
Lenecplusultra <yytbrayan@gmail.com>
2025-06-09 17:57:44 -0400

    modif to access backend

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Allow frontend to access backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://ai-doc-assistant.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
